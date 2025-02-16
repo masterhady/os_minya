2423c57d205a617840ad7b088f83334a7e765783
saramahmoud7689 <saramahmoud7689@gmail.com>
2025-02-16 13:04:55 +0200

    merge master with sara :

---
+++ Ahmed_Mohamed_Salah_Mohamed.js
@@ -0,0 +1,21 @@
+function Ahmed_Mohamed_Salah() {
+  var as = "Ahmed Mohamed Salah";
+}
+// what is realy full mistake is not to use them
+new line using ssh 
+function task4() {
+  var name = "Ahmed Mohamed Salah";
+}
+
+hellllllllllllllllllllllllllllllllllllloooooooooooo;
+function task5() {
+  var task5 = "dfvhjbdkfv";
+}
+new Branch();
+function task4() {
+  var name = "Ahmed Mohamed Salah";
+}
+
+hellllllllllllllllllllllllllllllllllllloooooooooooo;
+// new Branch
+// new line to check git stash
