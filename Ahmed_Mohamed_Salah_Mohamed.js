47cecbe8c2dec0c81f504201164ba8d63de145be
ahmed <ahmed@kali-Ahmed.kali-Ahmed>
2025-02-16 11:46:54 +0200

    no edit to be combared

--- Ahmed_Mohamed_Salah_Mohamed.js
+++ Ahmed_Mohamed_Salah_Mohamed.js
@@ -3,7 +3,7 @@ function Ahmed_Mohamed_Salah () {
 }
 // what is realy full mistake is not to use them
 function task4(){
-    var name="Ahmed Mohamed Salah"
+    var name = "Ahmed Mohamed Salah"
 }
 
 
